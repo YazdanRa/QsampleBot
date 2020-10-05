97379f6dd358395bcdffd9b48b2efcb20382b619
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-10-05 21:01:26 +0330

    Update users' model structure
    - Django users doesn't need for here!
    - Telegram has some fields for user to find out them checkout telegram API documentation
    - Remove extra comments

--- QBot/telegram_bot/models.py
+++ QBot/telegram_bot/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
 
 
-class User(AbstractUser):
-    chat_id = models.CharField(max_length=128, unique=True)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128, null=True)
-    username = models.CharField(max_length=128, unique=True)
+class User(models.Model):
+    chat_id = models.BigIntegerField(primary_key=True)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256, null=True, blank=True)
+    username = models.CharField(max_length=256, null=True, blank=True)
+    is_bot = models.BooleanField(default=False)
+    language_code = models.CharField(max_length=8, null=True, blank=True)
+    date_met = models.DateTimeField(auto_now_add=True)
+    last_seen = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return "{} {} (@{})".format(self.first_name, self.last_name, self.username)
